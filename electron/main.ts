2bd4e8ad74b28aca2a1a43f736d5f0820c3fc5f0
tuanmjnh <minhtuan200990@gmail.com>
2025-06-11 14:14:16 +0700

    2025.06.11

--- electron/main.ts
+++ electron/main.ts
@@ -21,14 +21,14 @@ process.env.APP_ROOT = path.join(__dirname, '..')
 export const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL']
 export const MAIN_DIST = path.join(process.env.APP_ROOT, 'dist-electron')
 export const RENDERER_DIST = path.join(process.env.APP_ROOT, 'dist')
-
+console.log('VITE_DEV_SERVER_URL:', VITE_DEV_SERVER_URL)
 process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL ? path.join(process.env.APP_ROOT, 'public') : RENDERER_DIST
-
+console.log('VITE_PUBLIC:', process.env.VITE_PUBLIC)
 let win: BrowserWindow | null
-
+console.log(path.join(process.env.VITE_PUBLIC, 'icon.png'))
 function createWindow() {
   win = new BrowserWindow({
-    icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
+    icon: path.join(process.env.VITE_PUBLIC, 'icon.png'),
     width: 1150,
     height: 768,
     show: true,
